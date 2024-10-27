823f4b16462ddba862b718dbaefd9a6f7c885d1c
dmneha <dmneha@ttu.ee>
2024-10-27 06:43:27 +0200

    welcome template

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
-
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/welcome')
+def contact():
+    return render_template('welcome.html')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=10000)
